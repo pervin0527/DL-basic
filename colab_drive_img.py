f0f8f67fabb178aa75acaa9720d81acf452a834b
pervin0527 <zfbtldnz@gmail.com>
2023-02-20 00:45:21 +0900

    Updated 2023.02.20

--- colab_drive_img.py
+++ colab_drive_img.py
@@ -1,8 +1,11 @@
-url="https://drive.google.com/file/d/1LdjOv8XIlu8-GTL_n-yySF3B-rPNEBhH/view?usp=share_link"
-path='https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-size="width=\"\" "+"height =\"\" "
-tag="<img src='"+path+"' "+size+"/><br>"
+def convert_url(url):    
+    path='https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+    size="width=\"\" "+"height =\"\" "
+    tag="<img src='"+path+"' "+size+"/><br>"
 
-url.split('/')
-path='https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-print(" ▶ Path : ", path)
+    url.split('/')
+    path='https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+    print(" ▶ Path : ", path)
+
+input_url = "https://drive.google.com/file/d/1OWpDF4PlSOnyGfLBQYI3V8RB8FW9okp2/view?usp=share_link"
+convert_url(input_url)
