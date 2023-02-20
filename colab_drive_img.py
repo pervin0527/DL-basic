9a067e6d39e03aca1e99e68df3c7952cec62777b
pervin0527 <zfbtldnz@gmail.com>
2023-02-20 22:21:37 +0900

    Updated 2023.02.20

--- colab_drive_img.py
+++ colab_drive_img.py
@@ -7,5 +7,5 @@ def convert_url(url):
     path='https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
     print(" ▶ Path : ", path)
 
-input_url = "https://drive.google.com/file/d/1OWpDF4PlSOnyGfLBQYI3V8RB8FW9okp2/view?usp=share_link"
+input_url = "https://drive.google.com/file/d/1-WsqF4J4CIDSgaZooBjnx-s3hP3Yu8Db/view?usp=share_link"
 convert_url(input_url)
