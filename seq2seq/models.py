58a188f8300c57c24a23d10e4bba494d4a753deb
pervin0527 <zfbtldnz@gmail.com>
2024-04-27 23:56:51 +0900

    Updated 2024.04.27

---
+++ seq2seq/models.py
@@ -0,0 +1,19 @@
+from torch import nn
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, embedding_dim, hidden_dim, n_layers, dropout):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.embedding = nn.Embedding(input_dim, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src):
+        embedded = self.dropout(self.embedding(src)) # src = [src length, batch size]
+        outputs, (hidden, cell) = self.lstm(embedded) # embedded = [src length, batch size, embedding dim]
+        # outputs = [src length, batch size, hidden dim * n directions]
+        # hidden = [n layers * n directions, batch size, hidden dim]
+        # cell = [n layers * n directions, batch size, hidden dim]
+        # outputs are always from the top hidden layer
+        return hidden, cell
